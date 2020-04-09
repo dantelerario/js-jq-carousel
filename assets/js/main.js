aff872adcaf007b332e389cc954b7aae00eb2248
Dante L'Erario <dantelerario@gmail.com>
2020-04-09 15:54:27 +0200

    JS started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,42 @@
 // js-jq-carousel
-console.log('OK') //debug
+
+$(document).ready( function() {
+
+  console.log('OK') //debug
+
+var arrowL = $('.arrow-left');
+
+var arrowR = $('.arrow-right');
+
+var img = $('.img');
+
+arrowL.click( function() {
+  console.log('cliccato');
+
+})
+
+arrowR.click( function() {
+  console.log('cliccato');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})

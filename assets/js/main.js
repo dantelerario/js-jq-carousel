59f18fdad716a388beaf1ae7812bf0b9f1a67710
Dante L'Erario <dantelerario@gmail.com>
2020-04-09 16:49:41 +0200

    carousel with buttons done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,44 @@ var arrowL = $('.arrow-left');
 
 var arrowR = $('.arrow-right');
 
-var img = $('.img');
+
+
+
 
 arrowL.click( function() {
-  console.log('cliccato');
+  console.log('cliccato');  //debug
+
+  var img = $('.active');
+  var pImg = img.prev();
+
+  if (img.hasClass('first')) {
+    img.hadClass('active');
+  }
+  else if (img.hasClass('active')) {
+    img.removeClass('active').addClass('hidden');
+    pImg.removeClass('hidden').addClass('active');
+  }
+
 
 })
 
 arrowR.click( function() {
-  console.log('cliccato');
+  console.log('cliccato');  //debug
+
+  var img = $('.active');
+  var nImg = img.next();
+
+  if (img.hasClass('last')) {
+    img.hadClass('active');
+  }
+  else if (img.hasClass('active')) {
+    img.removeClass('active').addClass('hidden');
+    nImg.removeClass('hidden').addClass('active');
+  }
+
 
 })
+
 
 
 
